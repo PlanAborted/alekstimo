56cbdc89f16df9b9f1e1c278909898a48e55d821
Aleks <aleks93t@gmail.com>
2020-01-30 23:01:45 +0100

    Updates

---
+++ precache-manifest.eb172253103ab568679ceb4cf2f5a24d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ddead888c43ddc295347fc9d99d266",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d02a45fa0478c2ed5a5",
+    "url": "/static/css/main.1fbdc4b5.chunk.css"
+  },
+  {
+    "revision": "9042d87705f4ded7dc87",
+    "url": "/static/js/2.0ec2e183.chunk.js"
+  },
+  {
+    "revision": "ad9e7d9c238b311d1af1c780913f93f9",
+    "url": "/static/js/2.0ec2e183.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5d02a45fa0478c2ed5a5",
+    "url": "/static/js/main.482985b8.chunk.js"
+  },
+  {
+    "revision": "7eaa71edefc03cd096e5",
+    "url": "/static/js/runtime-main.bb9910d2.js"
+  }
+]);
