9df6989ad0281d46a3260abbed8593da894c7239
Aleks <aleks93t@gmail.com>
2020-02-29 16:47:10 +0100

    Update footer

--- src/components/Page/PageFooter.js
+++ src/components/Page/PageFooter.js
@@ -5,7 +5,9 @@ import { Grid, Typography } from '@material-ui/core';
 const PageFooter = () => (
   <Grid container justify="center" alignItems="center">
     <Grid item>
-      <Typography variant="body1">Aleksandar Timotijevic</Typography>
+      <Typography variant="body1" justify="center">
+        Copyright (c) {new Date().getFullYear()} - Aleksandar Timotijevic
+      </Typography>
     </Grid>
   </Grid>
 );
