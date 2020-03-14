05a7382be8751a38f4243b78763ec657406c173c
Aleks <aleks93t@gmail.com>
2020-03-14 19:10:23 +0100

    Updating according to master branch build

---
+++ precache-manifest.71f5cd29792add354a1bb3e68da5d59f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8b9c7ac5709e503265b03ae09e1d2e3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4e19d647ce92fe00a097",
+    "url": "./static/js/2.ed304d0b.chunk.js"
+  },
+  {
+    "revision": "60278b717702ec5498a145b7f3d5b950",
+    "url": "./static/js/2.ed304d0b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19833a0caf95a8c308d7",
+    "url": "./static/js/main.70ece7a2.chunk.js"
+  },
+  {
+    "revision": "534c1046ca0e369bc4d8",
+    "url": "./static/js/runtime-main.2499ca74.js"
+  }
+]);
